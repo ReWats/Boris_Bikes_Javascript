7d18ca5f34a658fa2626e6987aa4b10fc63e79b0
Rebecca Watson <181154487+ReWats@users.noreply.github.com>
2025-02-02 16:18:01 +0000

    Create Payment.js
    Add js code

---
+++ Payment.js
@@ -0,0 +1,10 @@
+class Payment {
+    constructor() {
+      this.defaultCost = ("5");
+    }
+      calculateCost() {
+        this.defaultCost = (hourlyRate * rentalHours);
+      }
+  }
+
+  module.exports = Payment;
